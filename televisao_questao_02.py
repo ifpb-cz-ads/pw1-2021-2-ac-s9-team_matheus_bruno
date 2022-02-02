96157af8f88a27c02297088571ee7e3cddeed305
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-02-02 14:52:50 -0300

    Apagando arquivos desnecessários

--- televisao_questao_02.py
+++
@@ -1,9 +0,0 @@
-#2) Atualmente, a classe Televisão inicializa o canal com 2. Modifique a classe T
-# elevisão de forma a receber o canal inicial em seu construtor.
-
-class Televisão:
-    def __init__(self, canal_inicial):
-        self.canal_inicial = canal_inicial
-        self.tamanho = 30
-        self.marca = "Century"
-
