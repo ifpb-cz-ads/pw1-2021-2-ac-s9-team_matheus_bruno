a160ed49654d9565353e832ded4fe81e8b4388a4
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-02-02 13:30:29 -0300

    Resolvendo questão 01

---
+++ questao_01.py
@@ -0,0 +1,17 @@
+#1) Adicione os atributos tamanho e marca à classe Televisão. 
+# Crie dois objetos Televisão e atribua tamanhos e marcas diferentes. 
+# Depois, imprima o valor desses atributos de forma a confirmar a independência 
+# dos valores de cada instância (objeto).
+
+from televisao_questao_01 import Televisão
+
+tv_01 = Televisão()
+tv_01.tamanho = 50
+tv_01.marca = "LG"
+
+tv_02 = Televisão()
+tv_02.tamanho = 30
+tv_02.marca = "PHILIPS"
+
+print(f"----------Primeira TV----------\nTamanho: {tv_01.tamanho}\nMarca: {tv_01.marca}\n")
+print(f"----------Segunda TV----------\nTamanho: {tv_02.tamanho}\nMarca: {tv_02.marca}")
